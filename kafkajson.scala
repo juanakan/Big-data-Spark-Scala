db629ed2a1e6ecf33f72306d222c073e3bd9b239
juanakan <36744896+juanakan@users.noreply.github.com>
2020-02-04 15:51:06 +0100

    Update kafkajson.scala

--- kafkajson.scala
+++ kafkajson.scala
@@ -6,11 +6,15 @@ object kafkajson {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("kafkajson").master("local[2]").getOrCreate()
     val df = spark.readStream
+    
+    //declaramos el formato y las propiedades
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "topic1")
       .option("startingOffsets", "earliest")
-      .load() // castear los datos leidos en formato kafka para convertirlos en strings
+      .load()
+    
+    // castear los datos leidos en formato kafka para convertirlos en strings
     val res = df.selectExpr("CAST(value AS STRING)")
     val schema = new StructType()
       .add("id",IntegerType)
@@ -19,14 +23,15 @@ object kafkajson {
       .add("gender", StringType)
       .add("email", StringType)
       .add("ip_address", StringType)
-
-
+    
+    // hacemos la consulta que nos devuelve todos los datos
     val personal = res.select(from_json(col("value"), schema).as("data"))
       .select("data.*")
+    
+    //filtramos los datos por los dos nombres que no queremos
     val filter_df=personal.filter("first_name !='Willard' and first_name !='Noell'")
 
-
-
+    //mostramos los datos por consola
     print("mostrar los datos por consola")
     filter_df.writeStream
 
